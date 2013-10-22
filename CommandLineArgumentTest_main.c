8b46e818e5c2a388f431f1b9faafff61d56f1a49
Eric Gallager <egallager@comcast.net>
2013-10-22 08:18:28 -0400

    Update CommandLineArgumentTest_main.c
    elaborate on comment

--- CommandLineArgumentTest_main.c
+++ CommandLineArgumentTest_main.c
@@ -15,7 +15,7 @@ int main (int	argc,	/* input - argument count (including program name) */
     printf("\n argc is %i \n", argc);
 	for (i = 0; i < argc; i++) // Goes through each element of argv[]
 	{
-		printf("\n First character of argv[%i] is %c \n", i, *argv[i]); // Prints each entry in argv[] (Warning: %c can only display one character at a time. I'd use %s but that causes errors.)
+		printf("\n First character of argv[%i] is %c \n", i, *argv[i]); // Prints each entry in argv[] (Warning: %c can only display one character at a time. I would use %s instead, but that caused errors the last time I tried it.)
 	}
 	printf("\n All arguments displayed. \n");
     return 0;
